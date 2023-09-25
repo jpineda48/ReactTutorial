fdc1f192287bcba1fd3eb09253a9cd6e6f4402f8
jpineda48 <jacquelinepineda48@gmail.com>
2023-09-24 21:25:43 -0400

    notes added

---
+++ src/componants/app.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
+import { useState } from "react";
+
+
+function App(){
+
+    const [notes, setNotes] = useState ([]);
+    
+    function addNote(newNote) {
+        
+            setNotes(prevNotes => {
+              return  [...prevNotes, newNote];
+            });
+        }
+
+
+    
+     return (
+        <div>
+        <Header/>
+        <CreateArea onAdd={addNote}/>
+       { notes.map((noteItem) => {
+            return <Note title={noteItem.title} content = {noteItem.content} />
+        })}
+        <Footer/>
+    </div>
+    );
+}
+
+
+export default App;
---
+++ src/componants/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer(){
+
+    const currentyear = new Date().getFullYear()
+    return (
+        <footer>
+        <p>
+        Copyright C {currentyear}
+    </p>
+    </footer>
+    )
+}
+
+
+export default Footer;
